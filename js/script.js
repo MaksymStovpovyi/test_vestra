140bf68db0612d1b9e8298759def97c7c5275e64
maxSt <stovpovymax@gmail.com>
2023-07-01 00:40:55 -0400

    slider-beta @media

--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
-let topNavBtn = document.getElementsByClassName('top-menu');
+window.addEventListener('DOMContentLoaded', function() {
 
-function activeBtn() {
-  for (let i = 0; i < topNavBtn.length; i++) {
-    topNavBtn[i].classList.remove("active-menu");
+  // styles for btn of top menu
+
+  let topNavBtn = document.getElementsByClassName('top-menu');
+
+  function activeBtn() {
+    for (let i = 0; i < topNavBtn.length; i++) {
+      topNavBtn[i].classList.remove("active-menu");
+    }
+    this.classList.add("active-menu");
   }
-  this.classList.add("active-menu");
-}
 
-for(let i = 0; i < topNavBtn.length; i++) {
-  topNavBtn[i].addEventListener('click', activeBtn);
-}
+  for(let i = 0; i < topNavBtn.length; i++) {
+    topNavBtn[i].addEventListener('click', activeBtn);
+  }
+
+  //Slider
+
+  const slides = document.querySelectorAll('.slider-img'),
+        prev = document.querySelector('.prev-arrow'),
+        next = document.querySelector('.next-arrow'),
+        dotSlider = document.querySelectorAll('.slider-dots');
+
+  let sliderIndex = 0;
+
+  showSlides(sliderIndex);
+
+  function showSlides(x) {
+    if (x > slides.length - 1) {
+      sliderIndex = 0;
+    } 
+    if (x < 0) {
+      sliderIndex = slides.length - 1;
+
+    }
+    
+    slides.forEach(item => item.style.display = 'none');
+    slides[sliderIndex].style.display = 'block';
+
+    dotSlider.forEach(item => item.classList.remove('dot-act'));
+    dotSlider[sliderIndex].classList.add('dot-act');
+  }
+
+  function moveSlider(n) {
+    showSlides(sliderIndex += n);
+  } 
+
+  prev.addEventListener('click', () => {
+    moveSlider(-1);
+  })
+
+  next.addEventListener('click', () => {
+    moveSlider(1);
+  })
+
+
+});
